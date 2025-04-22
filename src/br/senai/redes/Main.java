2cb7a0d4b470083d0a0b5d9341ae2c714c02a79a
GABRIEL LACERDA CORREIA <25132902@CFP127ALUNO.LOCAL>
2025-04-22 09:56:16 -0300

    Automatizando processos para obter informações do ip

--- src/br/senai/redes/Main.java
+++ src/br/senai/redes/Main.java
@@ -8,11 +8,11 @@ public class Main {
 
 		IPinfo ip1 = new IPinfo();
 		
-		ip1.setCIDR(16);
+		ip1.setIP("168.198.0.0/24");
 		ip1.calculaHosts();
 		ip1.infoIP();
 		ip1.mostraResultado();
 		
 	}
-
+	
 }
--- src/br/senai/redes/model/IPinfo.java
+++ src/br/senai/redes/model/IPinfo.java
@@ -4,14 +4,20 @@ package br.senai.redes.model;
 import java.lang.Math;
 
 public class IPinfo {
+	private String IP;
 	private int cidr;
 	private int hosts;
 	private String classeIP;
 	private String mascaraPadrao;
 	private String mascaraBinario;
+	private String[] octetos;
+	
+	public void setIP(String IP) {
+		this.IP = IP;
+	}
 	
 	public void setCIDR(int cidr) {
-		this.cidr =cidr;
+		this.cidr = cidr;
 	}
 	
 	public void calculaHosts() {
@@ -19,21 +25,35 @@ public class IPinfo {
 		this.hosts = (int) Math.pow(2, hosts)- 2;
 	}
 	
-	public void infoIP() {
-		if (cidr == 8) {
-			classeIP = "Classe A";
-			mascaraPadrao = "255.0.0.0";
-			mascaraBinario = "11111111.00000000.00000000.00000000";
-		} else if (cidr == 16) {
-			classeIP = "Classe B";
-			mascaraPadrao = "255.255.0.0";
-			mascaraBinario = "11111111.11111111.00000000.00000000";
-		} else if (cidr == 24) {
-			classeIP = "Classe C";
-			mascaraPadrao = "255.255.255.0";
-			mascaraBinario = "11111111.11111111.11111111.00000000";
-		}
+	public void splitIP() {
+		
+		String[] splitIP = IP.split("/");
+		cidr = Integer.valueOf(splitIP[1]);
+		octetos = splitIP[0].split("\\.");
+		
 	}
+	
+	public void infoIP() {
+		int primeiroOcteto = Integer.valueOf(octetos[0]);
+		if (primeiroOcteto > 0 && primeiroOcteto < 172);
+	}
+	
+	
+//	public void infoIP() {
+//		if (cidr == 8) {
+//			classeIP = "Classe A";
+//			mascaraPadrao = "255.0.0.0";
+//			mascaraBinario = "11111111.00000000.00000000.00000000";
+//		} else if (cidr == 16) {
+//			classeIP = "Classe B";
+//			mascaraPadrao = "255.255.0.0";
+//			mascaraBinario = "11111111.11111111.00000000.00000000";
+//		} else if (cidr == 24) {
+//			classeIP = "Classe C";
+//			mascaraPadrao = "255.255.255.0";
+//			mascaraBinario = "11111111.11111111.11111111.00000000";
+//		}
+//	}
 
 	
 	public void mostraResultado() {
