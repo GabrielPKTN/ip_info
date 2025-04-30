c2841a9d8f3a6747929c30df8bbbe2ad84d683f9
GABRIEL LACERDA CORREIA <25132902@dsn127m1242373.cfp127aluno.local>
2025-04-30 11:08:25 -0300

    Aplicando layout da tela, e posicionando elementos

--- src/br/senai/redes/Main.java
+++ src/br/senai/redes/Main.java
@@ -1,20 +1,15 @@
 package br.senai.redes;
 
+import br.senai.redes.gui.MostrarResultados;
 import br.senai.redes.model.IPinfo;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		IPinfo ip1 = new IPinfo();
+		MostrarResultados tela = new MostrarResultados();
 		
-		ip1.setIP("168.198.0.0/24");
-		ip1.splitIP();
-		ip1.classeIP();
-		ip1.binario();
-		ip1.decimal();
-		ip1.calculaHosts();
-		ip1.resultados();
+		tela.criaTela("IP Info");
 		
 	}
 	
--- src/br/senai/redes/gui/MostrarResultados.java
+++ src/br/senai/redes/gui/MostrarResultados.java
@@ -1,5 +1,49 @@
 package br.senai.redes.gui;
 
+import java.awt.Container;
+
+import javax.swing.*;
+
 public class MostrarResultados {
+	
+	private JLabel labelIP;
+	
+	private JTextField textIP;
+	
+	private JList listInfoIP;
+	
+	private String nomeSistema;
+	
+	public void criaTela(String nomeSistema) {
+		
+		this.nomeSistema = nomeSistema;
+		JFrame tela = new JFrame();
+		
+		tela.setSize(600, 400);
+		tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		tela.setResizable(false);
+		tela.setLayout(null);
+		tela.setTitle(this.nomeSistema);
+		
+		Container container = tela.getContentPane();
+		
+		labelIP = new JLabel();
+		labelIP.setText("Insira o IP desejado para pegar informações: ");
+		labelIP.setBounds(150, 60, 300, 15); //Ocupa 350 da width, na altura 100 até 110.
+		
+		textIP = new JTextField();
+		textIP.setBounds(140, 80, 300, 30);
+		
+		listInfoIP = new JList();
+		listInfoIP.setBounds(140, 120, 300, 200);
+		
+		
+		container.add(labelIP);
+		container.add(textIP);
+		container.add(listInfoIP);
+		
+		tela.setVisible(true);
+		
+	}
 
 }
